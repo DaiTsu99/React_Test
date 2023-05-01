30537248dc17494b8bcc04b17767e3baa6b6e0f2
Vafa <vafanarong99@gmail.com>
2023-05-01 15:40:46 +0900

    setup gh-pages

--- src/Components/JSONPost.tsx
+++ src/Components/JSONPost.tsx
@@ -4,21 +4,36 @@ import axios from "axios";
 import Paginate from '../Paginate';
 
 const JSONPost = () => {
-   const [posts, setPosts] = useState<any[]>([])
+   const [posts, setPosts] = useState<any[]>([]);
    const [currentPage, setCurrentPage] = useState<number>(1);
    const [postsPerPage] = useState<number>(3);
+   const [users, setUserList] = useState<any[]>([]);
 
-   
+   const filterGo = async (id:any) => {
+      // let filterClient = axios.create({
+      //    baseURL: "https://jsonplaceholder.typicode.com/posts?userId="+ id
+      // })
+
+      let condition = 'posts?_limit=10' + '&userId=' + id.value;
+
+      let filteredClient = await client.get(condition);
+
+      setPosts(filteredClient.data);
+   }
 
     const client = axios.create({
-        baseURL: "https://jsonplaceholder.typicode.com/posts" 
+        baseURL: "https://jsonplaceholder.typicode.com/" 
     });
 
     useEffect(() => {
         const fetchPost = async () => {
-            let response = await client.get('?_limit=10');
+            let response = await client.get('posts?_limit=10');
+            let response2 = await client.get('users')
+            setUserList(response2.data);
+            // console.log(response2.data);
             setPosts(response.data);
-            console.log(response.data);
+            
+            // console.log(response.data);
          };
          fetchPost();
      }, []);
@@ -34,10 +49,22 @@ const JSONPost = () => {
      return (
       <div>
         <div className="posts-container flex flex-col mx-10">
+         <div className="flex flex-row gap-x-2 m-4 bg-teal-300 p-4">
+            <div>Filter by User Id: </div>
+            <select value="DEFAULT" onChange={e => filterGo(e.target)}>
+               <option value="DEFAULT" disabled>Please choose one</option>
+               {users.map((user)=> {
+                  return(
+                     <option key={user.id}>{user.id}</option>
+                  )
+               })}
+            </select>
+         </div>
          {currentPosts.map((post) => {
             return (
                <div className="mt-10 border-b-2 border-black bg-blue-200" key={post.id}>
                   <h2 className="text-xl font-semibold">{post.title}</h2>
+                  <div className="p-4 italic">UserId: {post.userId}</div>
                   <p className="post-body">{post.body}</p>
                   <div className="flex justify-end">
                   <div className="bg-red-200 w-fit p-4 hover:bg-red-400 rounded cursor-pointer">Dummy Button</div>
--- src/Components/WeatherMaster.tsx
+++ src/Components/WeatherMaster.tsx
@@ -41,17 +41,17 @@ const WeatherMaster = () => {
     useEffect(() => {
         const fetchData = async () => {
             navigator.geolocation.getCurrentPosition(function(position) {
-              // setLat(position.coords.latitude);
-              setLat(34.64640);
-              // setLong(position.coords.longitude);
-              setLong(135.78055);
+              setLat(position.coords.latitude);
+              // setLat(34.64640);
+              setLong(position.coords.longitude);
+              // setLong(135.78055);
             });
             if(lat!=undefined || long !=undefined){
               await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&lang=${lang}&APPID=${process.env.REACT_APP_API_KEY}`)
               .then(res => res.json())
               .then(async (result) => {
                 setData(result);
-                
+                await wrap();
                 setHasLoaded(true);
                 
                 console.log(result);
@@ -83,7 +83,13 @@ const WeatherMaster = () => {
           <Weather weatherData={data} language={lang}/>
           </div>
         ): (
-          <div></div>
+          <div className="p-10 flex flex-row font-semibold text-xl items-center"> 
+            <svg className="animate-spin -ml-1 mr-3 h-10 w-10 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            Loading Page
+        </div>
         )}
          
       </div>
